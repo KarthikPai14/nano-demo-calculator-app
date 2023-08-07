be31cdbc530d967a9378dc26884c42b3f47381ba
karthik <karthik.pai140203@gmail.com>
2023-08-07 10:18:43 +0530

    qwerty

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!',200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data=requests.get_json()
+    if 'first' in data and 'second' in data:
+        result=data["first"]+data["second"]
+        response=jsonify({"result":result})
+    return response,200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
+    if 'first' in data and 'second' in data:
+        result=data["first"]-data["second"]
+        response=jsonify({"result":result})
+    return response,200
     return ''
 
 if __name__ == '__main__':
